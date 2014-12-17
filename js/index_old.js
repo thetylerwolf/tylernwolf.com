97e93aea1e7a9a187ddea82c46c32df1e7728d00
Tyler Wolf <twolf@novus.com>
2014-12-17 18:00:38 -0500

    updated look and feel 12/17/14

--- js/index_old.js
+++ js/index_old.js
@@ -4,6 +4,8 @@ d3.json('/data/entries.json', function(data) {
   // var colors = ["#9e1010", "#9e2710", "#be8513", "#328e1f", "#20598d", "#4f1ca0"]
   var colors = ["#68B841","#D9AB27", "#C23D23", "#9E3CD9", "#2587CF", "#C22E2B"]
 
+  //['#dbb700','#9d2810'];
+
   // var randomColor = function() { return colors[parseInt(Math.random() * colors.length)]; };
 
   // var randomGradient = function() {
@@ -27,24 +29,40 @@ d3.json('/data/entries.json', function(data) {
 
   var entryRect = slider.selectAll('a').data(entries);
 
-  var entryRectEnter = entryRect.enter().append('a').attr('class','entry')
+  var entryRectEnter = entryRect.enter().append('a').append('div').attr('class','entry')
+
+  var entryRectDiv = entryRect.select('.entry')
+
+  var entryDate = entryRectEnter.append('div').attr('class','entry-date')
 
   entryRectEnter
-      .text(function(d) { return d.title + ' - ' + d.date; })
-      .attr('href', function(d) { return d.url; })
-      ;
+      // .style('height','0%')
+      // .text(function(d) { return d.title; })
+      .style('color', '#262626')
+
+  entryRect.attr('href', function(d) { return d.url; })
+
+  entryRectDiv
+    .transition().delay(750).duration(1000)
+      .style('background-color',function(d,i) { return colorScale(i); /*colors[i % colors.length]*/})
+      .style('height','30%')
+      .style('max-height','175px')
+      .style('color','#fff')
+
+  entryDate
+      .text(function(d) { return d.date; })
 
   var hoverWait = false;
 
-  slider.selectAll('a').each(function(d,i) {
+  entryRectDiv.each(function(d,i) {
     d3.select(this)
       .on('mousemove', function() {
 
         var leftPanel = d3.select('#panel-left');
         var sliderTop = parseInt(slider.style('top'));
 
-        leftPanel.select('.title').text(function() { return d.title });
-
+        leftPanel.select('.title').text(function() { return d.title })//.property('innerHTML',function() { return d.title })
+        // leftPanel.select('.date').text(function() { return d.date })
         leftPanel.select('.explanation').text(function() { return d.description })
 
         if (hoverWait) return;
@@ -75,5 +93,11 @@ d3.json('/data/entries.json', function(data) {
       })
 
     });
+
+  // slider.style('height', function(d) {
+  //   var newHeight = d3.sum(entryRect[0], function(d) { return d.offsetHeight})
+  //   return (newHeight < window.innerHeight) ? '100%' :
+  //   newHeight + 'px';
+  // })
 
 })
